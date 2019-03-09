b1573048c4b242688844c20dcd68bd5bb4fb09dc
0221 <1252153341@qq.com>
2019-03-09 12:00:37 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name'=>'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
             'email'=>'required|email',
             'introduction'=>'max:80',
+            'avatar'=>'mimes:bmp,jpeg,gif,png|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -36,6 +37,9 @@ class UserRequest extends FormRequest
         return ['name.required'=>'用户名不能为空😡',
             'name.between'=>'用户名限制在3~25字符内',
             'name.regex'=>'非法字符😡',
-            'name.unique'=>'该用户名已被使用',];
+            'name.unique'=>'该用户名已被使用',
+            'avatar.mime'=>'请上传可用的格式文件',
+            'avatar.dimensions'=>'图片分辨率不够哦/(ㄒoㄒ)/~~',
+        ];
     }
 }
