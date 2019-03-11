7574731f404626ef99c9a94b36a1fa4ff61258b0
0221 <1252153341@qq.com>
2019-03-11 10:30:10 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,8 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
-       $topics=Topic::where('category_id',$category->id)->with('user','category')->paginate(20);
+    public function show(Request $request,Category $category,Topic $topic){
+       $topics=$topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
        return view('topics.index',compact('topics','category'));
     }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -11,4 +11,25 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function scopeWithOrder($query,$order)
+    {
+      switch ($order){
+          case 'recent':
+              $query->recent();
+              break;
+          case 'recentReplied':
+              $query->recentReplied();
+              break;
+      }
+      return $query->with('user','category');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at','desc');
+    }
+
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at','desc');
+    }
 }
